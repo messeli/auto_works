4c981cc2bf4b61e5012524c9dfbee693da955017
Mehmet Selim Akay <mehmetselimakay@gmail.com>
2021-05-31 22:22:07 +0300

    Zero epsH zeta works featuring dats with XY - 1st commit

---
+++ duffingBN_backbone/duffingBN_backbone4.f90
@@ -0,0 +1,70 @@
+
+SUBROUTINE FUNC(NDIM,U,ICP,PAR,IJAC,F,DFDU,DFDP) 
+  IMPLICIT NONE
+  INTEGER, INTENT(IN) :: NDIM, IJAC, ICP(*)
+  DOUBLE PRECISION, INTENT(IN) :: U(NDIM), PAR(*)
+  DOUBLE PRECISION, INTENT(OUT) :: F(NDIM)
+  DOUBLE PRECISION, INTENT(INOUT) :: DFDU(NDIM,*), DFDP(NDIM,*)
+  DOUBLE PRECISION WN,K3_M,U1,U2,X,Y,OMEG 
+
+  WN   = PAR(1) 
+  K3_M = PAR(2) 
+  OMEG = PAR(3)
+
+  U1 = U(1)
+  U2 = U(2)
+  X  = U(3)
+  Y  = U(4)
+
+  F(1) =  U2
+  F(2) = -WN**2*U1 - K3_M*U1**3 !- 2*ZETA*WN*U2 + F_M*X
+  F(3) =  X + OMEG*Y - X*(X**2 + Y**2)    
+  F(4) = -OMEG*X + Y - Y*(X**2 + Y**2)
+END SUBROUTINE FUNC
+
+
+SUBROUTINE STPNT(NDIM,U,PAR,T) 
+  IMPLICIT NONE
+  INTEGER, INTENT(IN) :: NDIM
+  DOUBLE PRECISION, INTENT(INOUT) :: U(NDIM), PAR(*)
+  DOUBLE PRECISION, INTENT(IN) :: T
+  DOUBLE PRECISION WN,K3_M,U1,U2,TPI,X,Y,OMEG 
+  
+  WN   = 1.d0
+  K3_M = 1.d0
+  OMEG = 1.318d0 !|2pi/Per <<< from the Matlab dat file .
+
+  PAR(1) = WN 
+  PAR(2) = K3_M 
+  PAR(3) = OMEG
+
+  TPI=8*ATAN(1.0D0) ! = 2*pi 
+  PAR(11)= 4.77d0  !TPI/OMEG  ! Period = 2*pi/omeg
+  X = SIN(TPI*T) 
+  Y = COS(TPI*T)
+
+  ! U(1) = 0 !Leave this to the dat file    !Although we start from 0, we do IPS=2 periodic and sample at Omeg=1.
+  ! U(2) = 0 !Leave this to the dat file    !Although we start from 0, we do IPS=2 periodic and sample at Omeg=1.
+  ! U(3) = X
+  ! U(4) = Y
+END SUBROUTINE STPNT
+
+
+SUBROUTINE PVLS(NDIM,U,PAR)
+  IMPLICIT NONE
+  INTEGER, INTENT(IN) :: NDIM
+  DOUBLE PRECISION, INTENT(IN) :: U(NDIM)
+  DOUBLE PRECISION, INTENT(INOUT) :: PAR(*)
+  ! ALREADY MAX(U(1))
+END SUBROUTINE PVLS
+
+
+SUBROUTINE BCND 
+END SUBROUTINE BCND
+
+SUBROUTINE ICND 
+END SUBROUTINE ICND
+
+SUBROUTINE FOPT 
+END SUBROUTINE FOPT
+
